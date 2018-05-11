9ce93f6dc511eb6063a4a93685207433d9555470
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-05-12 00:06:40 +0200

    myApp: basic list example

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace myApp
 {
@@ -7,6 +8,12 @@ namespace myApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var names = new List<string> { "Denny", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
