056e2a82ad99e2ebd1e44bc352c69082de8a4fcd
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-05-12 00:19:11 +0200

    myApp: creating function to write list of values

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -10,9 +10,20 @@ namespace myApp
             Console.WriteLine("Hello World!");
 
             var names = new List<string> { "Denny", "Ana", "Felipe" };
-            foreach (var name in names)
+            writeList(names, "Hello ", "!");
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            writeList(names, "Hello ", "!");
+        }
+
+        static void writeList(List<string> list, String prefix, String suffix)
+        {
+            foreach (String s in list)
             {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
+                Console.WriteLine($"{prefix}{s.ToUpper()}{suffix}");
             }
         }
     }
