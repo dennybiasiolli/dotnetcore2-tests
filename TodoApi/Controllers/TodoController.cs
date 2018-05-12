c2ac38decc2741f306815e802e19d73c20ed909c
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-05-12 09:29:05 +0200

    TodoApi: adding post method

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -37,5 +37,19 @@ namespace TodoApi.Controllers
             }
             return Ok(item);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
     }
 }
