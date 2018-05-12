5f5b5b2503417b089b1a63712122f3f4dda0fb8e
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-05-12 09:13:14 +0200

    TodoApi: adding get methods

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -20,5 +20,22 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
+        [HttpGet]
+        public List<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+        }
     }
 }
